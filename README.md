881ce3b79ef4699482ba55b0d69a89ae7f60cb06
John muller <phychicjosh@gmail.com>
2025-08-10 14:46:57 +0300

    finished the appointment page

--- README.md
+++ README.md
@@ -34,3 +34,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+PROJECT_ID=689739de002c9fdca3ce
+API_KEY=standard_be68677f9f3e91ada7c71aae99287113b1ce58b89482b9850b38ae5adfd04e650650e6cd0f364f0fcc3b03e7cb9d265c65dcdf9ff991328900d50cbbd756fa43b5176435e0e7051d6296c18f8b665c1b9c47d3cb133b59261246640585d61d49d3a5bc83aacd1e108ae59e15ed9a2454a8a7216954999b95a24fee4f51245c35
+DATABASE_ID=68977f8c001db461a213
+PATIENT_COLLECTION_ID=68977fb1002b5a0a12b1
+DOCTOR_COLLECTION_ID=68977ff1001f187dc92b
+APPOINTMENT_COLLECTION_ID=689780220001d9e926d4
+NEXT_PUBLIC_BUCKET_ID=68978079002c429a9d28
+NEXT_PUBLIC_ENDPOINT=https://fra.cloud.appwrite.io/v1
+
+
--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -14,6 +14,16 @@ import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { Input } from "@/components/ui/input";
 import { E164Number } from "libphonenumber-js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface CustomProps {
   control: Control<any>;
@@ -23,6 +33,7 @@ interface CustomProps {
   placeholder?: string;
   iconSrc?: string;
   iconAlt?: string;
+  disabled?: boolean;
   dateFormat?: string;
   showTimeSelect?: boolean;
   children?: React.ReactNode;
@@ -30,7 +41,15 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+  const {
+    fieldType,
+    iconSrc,
+    iconAlt,
+    placeholder,
+    showTimeSelect,
+    dateFormat,
+    renderSkeleton,
+  } = props;
   switch (fieldType) {
     case FormFieldType.INPUT:
       return (
@@ -67,6 +86,70 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           />
         </FormControl>
       );
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className={"flex rounded-md border border-dark-500 bg-dark-400"}>
+          <Image
+            src={"/assets/icons/calendar.svg"}
+            alt={"calendar"}
+            width={24}
+            height={24}
+            className={"ml-2"}
+          />
+          <FormControl>
+            <DatePicker
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+              dateFormat={dateFormat ?? "MM/dd/yyyy"}
+              showTimeSelect={showTimeSelect ?? false}
+              timeInputLabel={"Time:"}
+              wrapperClassName={"date-picker"}
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.SKELETON:
+      return renderSkeleton ? renderSkeleton(field) : null;
+    case FormFieldType.SELECT:
+      return (
+        <FormControl>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger className={"shad-select-trigger"}>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent className={"shad-select-content"}>
+              {props.children}
+            </SelectContent>
+          </Select>
+        </FormControl>
+      );
+    case FormFieldType.TEXTAREA:
+      return (
+        <Textarea
+          placeholder={placeholder}
+          {...field}
+          className={"shad-textArea"}
+          disabled={props.disabled}
+        />
+      );
+    case FormFieldType.CHECKBOX:
+      return (
+        <FormControl>
+          <div className={"flex items-center gap-4"}>
+            <Checkbox
+              id={props.name}
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+            <label htmlFor={props.name} className={"checkbox-label"}>
+              {props.label}
+            </label>
+          </div>
+        </FormControl>
+      );
+
     default:
       break;
   }
